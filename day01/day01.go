070be066608d056357f2f7289ed6c9888f1926fc
Petr Brantalík <grounipteam@gmail.com>
2023-12-01 09:42:49 +0100

    day1 wip

--- day01/day01.go
+++ day01/day01.go
@@ -1,8 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"strconv"
+)
+
+func GetCalibrationValue(input string) int {
+	var sum []string
+	for _, char := range input {
+		if char > 48 && char < 58 {
+			number := string(char)
+			sum = append(sum, number)
+		}
+	}
+
+	value := ""
+
+	length := len(sum)
+	switch length {
+	case 0:
+		value = "0"
+	case 1:
+		value = sum[0] + sum[0]
+	default:
+		value = sum[0] + sum[len(sum)-1]
+
+	}
+
+	parsedValue, err := strconv.Atoi(value)
+	if err != nil {
+		return 0
+	}
+
+	return parsedValue
+}
+
+func GetSumOfCalibrationValues(input []string) int {
+	sum := 0
+	for _, string := range input {
+		value := GetCalibrationValue(string)
+		sum += value
+	}
+
+	return sum
+}
+
+func ScanAndReturn(filepath string) []string {
+	return []string{"", ""}
+}
 
 func main() {
-	day := "01"
-	fmt.Println("Hello " + day)
+	GetCalibrationValue("1abc2")
 }
--- day01/day01_test.go
+++ day01/day01_test.go
@@ -1,6 +1,66 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestDay01(t *testing.T) {
+const (
+	validValue   = "1abc2"
+	invalidValue = "asd"
+	singleValue  = "asds1"
+)
+
+var exampleValue = []string{
+	"1abc2",
+	"pqr3stu8vwx",
+	"a1b2c3d4e5f",
+	"treb7uchet",
+}
+
+func TestGetCalibration(t *testing.T) {
+	t.Run("Get Calibration values", func(t *testing.T) {
+		got := GetCalibrationValue(validValue)
+		want := 12
+
+		if got != want {
+			t.Errorf("want %v but got %v", got, want)
+		}
+	})
+
+	t.Run("Get Calibration values from invalid", func(t *testing.T) {
+		got := GetCalibrationValue(invalidValue)
+		want := 0
+
+		if got != want {
+			t.Errorf("want %v but got %v", got, want)
+		}
+	})
+
+	t.Run("Get Calibration values from single ", func(t *testing.T) {
+		got := GetCalibrationValue(singleValue)
+		want := 11
+
+		if got != want {
+			t.Errorf("want %v but got %v", got, want)
+		}
+	})
+}
+
+func TestGetSumOfCalibrationValues(t *testing.T) {
+	got := GetSumOfCalibrationValues(exampleValue)
+	want := 142
+
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, exampleValue)
+	}
+}
+
+func TestScanAndReturn(t *testing.T) {
+	got := ScanAndReturn("testValues")
+	want := exampleValue
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("want %v but got %v", want, got)
+	}
 }
